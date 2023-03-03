84102454a2db9c624a2adfee22e4d3f72ce05645
leonid753 <125366925+leonid753@users.noreply.github.com>
2023-03-03 21:04:34 +0700

    р
    Добавил ходьбу прыжок поворот камеры

---
+++ My project (2)/Assets/PlayerMove.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce;
+    [SerializeField] float gravity;
+    [SerializeField] float shift;
+    [SerializeField] Animator anim;
+    private Vector3 direction;
+    
+    
+    void Start()
+    {
+
+    }    
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        if (controller.isGrounded)
+        {
+            direction = new Vector3(moveHorizontal, 0, moveVertical);
+            direction = transform.TransformDirection(direction) * speed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                direction.y = jumpForce;
+            }
+            if (Input.GetKey(KeyCode.LeftAlt))
+            {
+                //direction.y = -shift;
+                controller.height = 1.0f;
+            }
+            else 
+            {
+                controller.height = 2.0f;
+            }
+        }
+        
+        direction.y -= gravity * Time.deltaTime;
+        controller.Move(direction * Time.deltaTime);
+
+       // if (Input.GetMousButtonDown(0))
+        //{
+            //anim.SetTrigger("atac");
+        //}
+    }
+}
